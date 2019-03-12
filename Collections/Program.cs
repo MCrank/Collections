893b0a692420420ab1b8fd90dc1065f177a971e3
MCrank <marco_mcse@hotmail.com>
2019-03-11 19:23:20 -0500

    Collections Overview

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collections
 {
@@ -6,7 +7,33 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var studentsWithBlackHair = new List<string>() { "Nathan", "Austin", "Marty McFly" };
+
+            // Dictionary
+            var studentByHairColor = new Dictionary<string, List<string>> // Key Value pair (TKey, TValue).  Key can only be listed once
+            {
+                {"Black", studentsWithBlackHair }
+            }; 
+
+            studentByHairColor.Add("Bald", new List<string> { "Martin" });
+
+            var theBlackHairedStudents = studentByHairColor["Black"];
+
+            if (theBlackHairedStudents == studentsWithBlackHair)
+            {
+                Console.WriteLine("They are the same");
+            }
+
+            // Looping on a Dictionary returns a key value pair
+            foreach (var (hairColor, students) in studentByHairColor) // Deconstruct the Key Value Pair
+            {
+                Console.WriteLine($"The following students have {hairColor} hair.");
+                foreach (var student in students)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
